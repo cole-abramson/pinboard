9c831d9c7704a20e95ce299e91d41d3f190f4017
Cole Abramson and Oliver Switzer <pair+cole+oliver@dahmlabs.com>
2020-05-10 00:05:43 -0400

    Start with place holder cat that gets overwritten

--- server/app.rb
+++ server/app.rb
@@ -1,15 +1,13 @@
-require 'rubygems'
-require 'bundler'
 require 'sinatra'
-Bundler.setup(:default)
+require 'sinatra/json'
 
-@current_content = "https://placekitten.com/1000/800"
+current_content = "https://placekitten.com/1000/800"
 
 post '/sms' do
   content_type 'text/xml'
-  @current_content = params['Body']
+  current_content = params['Body']
 end
 
 get '/' do
-  @current_content
+  json current_content
 end
